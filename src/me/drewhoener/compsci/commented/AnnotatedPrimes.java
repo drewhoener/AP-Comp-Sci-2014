8a66fbf9296fcd99f1eb672e99fbba1156117257
Drew Hoener <drewhoener@gmail.com>
2014-09-25 07:30:49 -0400

    Removed some redundancy

--- src/me/drewhoener/compsci/commented/AnnotatedPrimes.java
+++ src/me/drewhoener/compsci/commented/AnnotatedPrimes.java
@@ -1,13 +1,11 @@
-package me.drewhoener.compsci.plainclass;
+package me.drewhoener.compsci.commented;
 
 import java.util.ArrayList;
 
-public class Primes {
-
-    static boolean debug = true;
+public class AnnotatedPrimes {
 
     public static void main(String[] args) {
-        Primes p = new Primes();
+        AnnotatedPrimes p = new AnnotatedPrimes();
 
         for (int i : p.getPrimes(50)) {
             System.out.println(i);
@@ -16,12 +14,13 @@ public class Primes {
     }
 
     public ArrayList<Integer> getPrimes(int before) {
+        boolean prime = false;
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(2);//because 2 is a weird prime number, get that in there first
 
         for (int num = 3; num <= before; num++) {
 
-            if (isPrime(num)) {
+            if (getFactors(num).size() > 2) {
                 list.add(num);
             }
 
@@ -30,16 +29,26 @@ public class Primes {
         return list;
     }
 
-    public boolean isPrime(int num) {
+    public ArrayList<Integer> getFactors(int num) {
+        boolean prime = true;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+
+        if (num != 1) list.add(num);
 
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
-                return false;
+                prime = false;
             }
         }
 
+        if (prime) {
+            list.add(num);
+        }
 
-        return true;
+
+        return list;
     }
 
 }
