55acb44fef1a522472161f65da101e50d5a9f1cd
Drew Hoener <drewhoener@gmail.com>
2014-10-06 07:27:11 -0400

    pushed some crap

---
+++ src/me/drewhoener/compsci/plainclass/Corrections.java
@@ -0,0 +1,8 @@
+package me.drewhoener.compsci.plainclass;
+
+public class Corrections {
+
+    public static void main(String[] args) {
+        System.out.println((23 % 5 + 31 / 4 % 3 - 17 % (16 % 10)));
+    }
+}
