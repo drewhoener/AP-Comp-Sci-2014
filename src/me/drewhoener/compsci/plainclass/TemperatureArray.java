ef1402a3901f114c1af946348054a230f0d10d15
Drew Hoener <drewhoener@gmail.com>
2014-10-17 10:18:28 -0400

    Temperature Array

---
+++ src/me/drewhoener/compsci/plainclass/TemperatureArray.java
@@ -0,0 +1,68 @@
+package me.drewhoener.compsci.plainclass;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TemperatureArray {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What were the temperatures?");
+        String[] strings = scanner.nextLine().split(" ");
+        double[] temperatures = new double[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+
+            try {
+                temperatures[i] = Double.parseDouble(strings[i]);
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                System.out.println("You entered things that weren't numbers! Shame on you!");
+
+            }
+
+        }
+
+        double avg = 0;
+
+        Arrays.sort(temperatures);
+
+        System.out.print("Your temperatures: ");
+
+        for (int i = 0; i < temperatures.length; i++) {
+
+            avg += temperatures[i];
+
+            System.out.print(temperatures[i] + " ");
+
+        }
+
+        System.out.println();
+
+        avg /= temperatures.length;
+
+        System.out.println("Temperature Average: " + avg);
+
+
+        int days = 0;
+
+        for (int i = 0; i < temperatures.length; i++) {
+
+            if (temperatures[i] > avg) {
+                days++;
+            }
+
+        }
+
+        System.out.print("Number of days above average: " + days);
+
+        System.out.println();
+
+        System.out.println("Two Hottest Days: " + temperatures[temperatures.length - 1] + ", " + temperatures[temperatures.length - 2]);
+
+
+    }
+}
