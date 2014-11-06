736282b31c0d939cd3b42d8c76f709403ac08f04
Lukas Metlicka <lmetlicka@g.harvard.edu>
2014-11-06 14:02:08 -0500

    Started HelloEnums with burns on max

---
+++ src/me/lukasmetlicka/compsci/advanced/HelloEnums.java
@@ -0,0 +1,13 @@
+package me.lukasmetlicka.compsci.advanced;
+
+/**
+ * Created by LukasMetlicka on 11/6/14.
+ */
+public class HelloEnums {
+    public enum MaxsGender {
+        MALE, FEMALE, NEUTRAL, ASEXUAL
+    }
+    MaxsGender max = MaxsGender.NEUTRAL;
+
+}
+
