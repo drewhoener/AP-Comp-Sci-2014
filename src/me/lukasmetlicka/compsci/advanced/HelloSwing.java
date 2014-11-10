63190c447be81d7edc4fc07f22db05b8b05f0384
Drew Hoener <drewhoener@gmail.com>
2014-11-10 08:37:07 -0500

    Proper panneling.

--- src/me/lukasmetlicka/compsci/advanced/HelloSwing.java
+++ src/me/lukasmetlicka/compsci/advanced/HelloSwing.java
@@ -9,13 +9,13 @@ public class HelloSwing {
 
     public static void main(String[] args){
 
-        JFrame frame = new JFrame("TestWindow");
+        /*JFrame frame = new JFrame("TestWindow");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200,1200);
         JButton b = new JButton("Click!");
         frame.add(b);
         frame.setBackground(Color.black);
-        frame.setVisible(true);
+        frame.setVisible(true);*/
 
 
 	    //Make Frame
@@ -25,6 +25,24 @@ public class HelloSwing {
 	    //add button to panel
 	    //add panel to frame.
 	    //Don't set the background of the frame itself. Always change the panel background
+
+	    JFrame mainFrame = new JFrame("TestingWindow");
+
+	    JPanel firstPanel = new JPanel();
+
+	    firstPanel.setPreferredSize(new Dimension(600, 600));
+	    firstPanel.setBackground(Color.BLACK);
+
+	    JButton button = new JButton("Click!");
+
+	    firstPanel.add(button);
+
+	    mainFrame.add(firstPanel);
+
+	    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    mainFrame.pack();
+	    mainFrame.setVisible(true);
+
 
 
     }
