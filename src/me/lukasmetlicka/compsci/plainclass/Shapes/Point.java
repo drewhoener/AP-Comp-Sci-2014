6d66c2ea3bfd0791527c69efb74cedfe117569ab
Lukas Metlicka <lmetlicka@g.harvard.edu>
2014-11-14 13:26:50 -0500

    Started Shapes Package

---
+++ src/me/lukasmetlicka/compsci/plainclass/Shapes/Point.java
@@ -0,0 +1,39 @@
+package me.lukasmetlicka.compsci.plainclass.Shapes;
+
+public class Point {
+
+	private int x;
+	private int y;
+
+	//Constructors
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public Point(){
+		this(0,0);
+	}
+
+	//Setters
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+
+	//Getters
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+
+	//Overrides
+	@Override
+	public String toString(){
+		return "X: "+ x + "\tY: "+ y;
+	}
+
+}
---
+++ src/me/lukasmetlicka/compsci/plainclass/Shapes/Quadrilateral.java
@@ -0,0 +1,13 @@
+package me.lukasmetlicka.compsci.plainclass.Shapes;
+
+public class Quadrilateral {
+
+	//Constructors
+	public Quadrilateral(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4){
+		Point p1 = new Point(x1, y1);
+		Point p2 = new Point(x2, y2);
+		Point p3 = new Point(x3, y3);
+		Point p4 = new Point(x4, y4);
+	}
+
+}
